8b2d754455d3f77a9faebab4e7552fec50482371
Arne Beer <arne@twobeer.de>
2014-04-05 19:56:08 +0200

    Everything should work fine right now. System commented

--- lovetoys/system.lua
+++ lovetoys/system.lua
@@ -19,9 +19,12 @@ function System:draw() end
 function System:requires() return {} end
 
 function System:addEntity(entity, category)
+    -- If there are multiple requirement lists, the added entities will 
+    -- be added to their respetive list. 
     if category then
         self.targets[category][entity.id] = entity
     else
+    -- Otherwise they'll be added to the normal self.targets list
         self.targets[entity.id] = entity
     end
 end
@@ -31,8 +34,11 @@ function System:removeEntity(entity)
         if table.firstElement(self.targets).__name then
             self.targets[entity.id] = nil
         else
-            for index, value in pairs(self:requires()) do
-                self.targets[index][entity.id] = nil
+        -- Removing entities from their respective category target list.
+            if self.targets[index] then 
+                for index, value in pairs(self:requires()) do
+                    self.targets[index][entity.id] = nil
+                end
             end
         end
     end
