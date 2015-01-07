2ff8f11b63a86defc1f941287ca607acb46cece7
Rafael Epplee <raffomania@gmail.com>
2015-01-07 21:49:40 +0100

    Fixed Entity:setParent not updating the parent's children table

--- src/entity.lua
+++ src/entity.lua
@@ -52,7 +52,9 @@ function Entity:remove(name)
 end
 
 function Entity:setParent(parent)
+    if self.parent then self.parent.children[self.id] = nil end
     self.parent = parent
+    self:registerAsChild()
 end
 
 function Entity:getParent(parent)
@@ -60,7 +62,7 @@ function Entity:getParent(parent)
 end
 
 function Entity:registerAsChild()
-    self.parent.children[self.id] = self
+    if self.id then self.parent.children[self.id] = self end
 end
 
 function Entity:get(name)
