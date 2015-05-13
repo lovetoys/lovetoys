22f669211601f753d81f595c7dc00917ecc474d4
Rafael Epplee <rafael.epplee@sinnerschrader.com>
2015-05-13 10:25:08 +0200

    Add Component.load()

--- src/component.lua
+++ src/component.lua
@@ -1,6 +1,21 @@
+-- Metaclass for Components
+-- In the future, this will be used to dynamically create Component classes
+-- Also has utility functions regarding Component handling
 Component = class("Component")
 
--- Just a superclass to identify components
+-- TODO this should create a new Component class
 function Component:__init()
+
 end
 
+-- Load multiple components and populate the calling functions namespace with them
+function Component.load(paths)
+  local env = {}
+  setmetatable(env, {__index = _G})
+  setfenv(2, env)
+
+  for _,path in pairs(paths) do
+    local componentName = string.match(path, "[^/]+$")
+    env[componentName] = require(path)
+  end
+end
