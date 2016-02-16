6a5be884b76030008088c570d3d39e953e72ad59
Rafael Epplee <raffomania@gmail.com>
2016-02-16 20:25:11 +0100

    Add component.load spec

--- spec/component_spec.lua
+++ spec/component_spec.lua
@@ -1,13 +1,31 @@
-require 'lovetoys'
-
 describe('Component', function()
-  it(':create with defaults creates a Component with default values', function()
-    local c = Component.create('TestComponent',
-      {'defaultField', 'emptyField'},
-      {defaultField = 'defaultValue'})
+    -- Reset Component.all before every test
+    before_each(function()
+        require('lovetoys')
+    end)
 
-    local instance = c()
-    assert.are.equal(instance.defaultField, 'defaultValue')
-    assert.is_nil(instance.emptyField)
-  end)
+    it(':create with defaults creates a Component with default values', function()
+        local c = Component.create('TestComponent',
+          {'defaultField', 'emptyField'},
+          {defaultField = 'defaultValue'})
+
+        local instance = c()
+        assert.are.equal(instance.defaultField, 'defaultValue')
+        assert.is_nil(instance.emptyField)
+    end)
+
+    it(':load returns the specified components', function()
+        local c1 = Component.create('TestComponent1')
+        local c2 = class('TestComponent2')
+        Component.register(c2)
+        local c3 = class('TestComponent3')
+
+        local loaded1, loaded2, loaded3 = Component.load({
+            'TestComponent1', 'TestComponent2', 'TestComponent3'
+        })
+
+        assert.are.equal(loaded1, c1)
+        assert.are.equal(loaded2, c2)
+        assert.is_nil(loaded3)
+    end)
 end)
